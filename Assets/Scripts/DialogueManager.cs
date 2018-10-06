feb57ac7fa11fddf9dad2698815eb9651a2281d0
Akynna <tim.nguyen@epfl.ch>
2018-10-06 14:31:31 +0200

    Added a Dialog System base :speech_balloon:

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+	}
+
+	public void startDialogue(Dialogue dialogue) 
+	{
+		// Print in the console the start of conversation
+		//Debug.Log("Starting conversation with " + dialogue.name);
+
+		// Set the character's name
+		nameText.text = dialogue.name;
+
+		// Clear previous messages
+		sentences.Clear();
+
+		// Add the new next sentences in the queue
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		// Display the next sentence
+		displayNextSentence();
+
+	}
+
+	public void displayNextSentence()
+	{
+		// Check if there is more sentences in the queue
+		if(sentences.Count == 0)
+		{
+			endDialogue();
+		}
+
+		// Collect the next sentence
+		string sentence = sentences.Dequeue();
+		//Debug.Log(sentence);
+		dialogueText.text = sentence;
+	}
+
+	void endDialogue()
+	{
+		Debug.Log("End of conversation.");
+	}
+
+}
